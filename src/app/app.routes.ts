0c03df0b9e5adea019b1d5a40cf6aa4ec9a8d061
GUERMITICHAWKI <Guermitichawki@gmail.com>
2026-02-11 11:51:33 +0100

    admin routin +module

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [{ path: 'visiteur', loadChildren: () => import('./visiteur/visiteur.module').then(m => m.VisiteurModule) }];
+export const routes: Routes = [{ path: 'visiteur', loadChildren: () => import('./visiteur/visiteur.module').then(m => m.VisiteurModule) },
+                               { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) }];
 @NgModule({
 imports: [RouterModule.forRoot(routes,
 {preloadingStrategy: PreloadAllModules}
